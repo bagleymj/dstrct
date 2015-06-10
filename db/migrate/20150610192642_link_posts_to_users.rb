faf938c3efc01cc6a49598cef05c8f7dd375ef5c
Mitchell Bagley <bagleymj@gmail.com>
2015-06-10 15:30:52 -0400

    Created users controller and model

---
+++ db/migrate/20150610192642_link_posts_to_users.rb
@@ -0,0 +1,7 @@
+class LinkPostsToUsers < ActiveRecord::Migration
+  def change
+    change_table :posts do |t|
+      t.belongs_to :user
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610183750) do
+ActiveRecord::Schema.define(version: 20150610192642) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
     t.string   "content"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
