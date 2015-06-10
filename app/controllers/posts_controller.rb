df38e29395d4910b3a67106950ed9d2753b1b5e2
Mitchell Bagley <bagleymj@gmail.com>
2015-06-10 14:48:46 -0400

    Added a rudimentary post creation form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+  def index
+    @greeting = "Hello World"
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+
+  end
+  
+  def show
+
+  end
+  
+  def edit
+ 
+  end
+  
+  def update
+
+  end
+  
+  def delete
+
+  end
+  
+  def destroy
+
+  end
+
 end
