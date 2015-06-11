e0cecb3b36ae50b8c16449b8e3a33e51201a43f7
Mitchell Bagley <bagleymj@gmail.com>
2015-06-11 09:06:27 -0400

    Added sign up function and password hashing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610192642) do
+ActiveRecord::Schema.define(version: 20150610195359) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20150610192642) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
