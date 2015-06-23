bfc71953df89d1880112ab3b1047c368f728f1be
Mitchell Bagley <bagleymj@gmail.com>
2015-06-23 15:33:46 -0400

    Started working on a more granular countdown

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def humanize(countdown)
+    hours_and_seconds = countdown.divmon(3600)
+    minutes_and_seconds = hours_and_seconds.divmon(60)
+    @hours = hours_and_seconds[0]
+    @minutes = minutes_and_seconds[0]
+    @seconds = minutes_and_seconds[1]
+  end
 end
