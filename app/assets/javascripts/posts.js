53a5054601560533321efbb01e003ddf266fb7f7
Mitchell Bagley <bagleymj@gmail.com>
2015-06-29 15:57:35 -0400

    Playing with JavaScript and not making much progress

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,8 +1,8 @@
-$(document).ready( function () {
+/*$(document).ready( function () {
   var count = 1000;
   $(".countdown").text(count);
   var countdown = setInterval( function () {
     $(".countdown").text(count);
     count -= 1;
   },1000);
-});
+});*/
